43d251b0bed0fc5ad3b3758dafe72f67129b1216
OwenMathieson <owen.mathieson@my.jcu.edu.au>
2017-08-30 15:57:50 +1000

    Proof of concept for L fixed

--- SongsToLearn.py
+++ SongsToLearn.py
@@ -1,25 +1,29 @@
 import csv
-
+number_of_songs = 6
 
 def main():
-    menu = """Songs To Learn - 1.0 By Owen Mathieson
-    L - List of Songs
-    A - Add New Song
-    C - Complete A Song
-    Q - Quit"""
-
-    print(menu)
-
-    choice = input(">>> ").upper()
-
+    choice = ""
     while choice != "Q":
+        print("""Songs To Learn - 1.0 By Owen Mathieson
+        {} Songs Loaded
+        L - List of Songs
+        A - Add New Song
+        C - Complete A Song
+        Q - Quit""".format(number_of_songs))
+
+
+
+        choice = input(">>> ").upper()
+
+
         if choice == "L":
-            printsongs = csv.DictReader('Songs.csv', delimiter=',')
-            for line in printsongs:
-                print(line["Song"]),
-                print(line["Artist"])
-                print(line["Year"])
-                print(line["Learned?"])
+            with open('SongList.csv') as songreader:
+                songs = list(csv.reader(songreader, delimiter=','))
+                for i in range(0, len(songs), 1):
+                    print(i, '. {}'.format(songs[i]))
+
+
+
 
 
         elif choice == "A":
