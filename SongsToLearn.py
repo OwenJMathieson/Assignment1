53d7fef8ee89362b1f7ce55ca26b789640299e6b
OwenMathieson <owen.mathieson@my.jcu.edu.au>
2017-08-30 17:24:06 +1000

    Proof of concept for L fixed

--- SongsToLearn.py
+++ SongsToLearn.py
@@ -17,17 +17,50 @@ def main():
 
 
         if choice == "L":
-            with open('SongList.csv') as songreader:
-                songs = list(csv.reader(songreader, delimiter=','))
-                for i in range(0, len(songs), 1):
-                    print(i, '. {}'.format(songs[i]))
+            songreader = open('SongList.csv')
+            songs = list(csv.reader(songreader, delimiter=','))
+            for i in range(0, len(songs), 1):
+                if songs[i][3] == 'Y':
+                    songs[i][3] = '*'
+                else:
+                    songs[i][3] = ''
+                print(i, '. {:1} {:30} {:1} {:30} {} {} {}'.format(songs[i][3], songs[i][0], '- ', songs[i][1],'(', songs[i][2], ')'))
 
 
 
 
 
         elif choice == "A":
-            print("Hello")
+            title = str(input("Title:"))
+            while title == "":
+                print("Input cannot be blank.")
+                title = str(input("Title:"))
+
+
+            artist = str(input("Artist:"))
+            while artist == "":
+                print("Input cannot be blank.")
+                artist = str(input("Artist:"))
+
+            year = input("Year:")
+            while year != int or year <= 0 or year == "":
+                print("Invalid Input; enter a valid number")
+                year = input("Year:")
+
+            new_song = "{}, {}, {}, N".format(title, artist, year)
+            opencsv = open('SongList.csv')
+            reader = csv.reader(opencsv)
+            writer = csv.writer(opencsv, delimiter=",", quotechar=new_song, quoting=csv.QUOTE_ALL)
+            for row in reader:
+                writer.writerow(row)
+
+
+
+
+
+
+
+
 
 
         elif choice == "C":
