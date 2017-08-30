2761ada4a088845002d20d56a09a51a4636ce838
OwenMathieson <owen.mathieson@my.jcu.edu.au>
2017-08-30 13:13:24 +1000

    Added csv file, Added menu and functionality for list display

--- SongsToLearn.py
+++ SongsToLearn.py
@@ -1,1 +1,37 @@
-print("Hello World")
+import csv
+
+
+def main():
+    menu = """Songs To Learn - 1.0 By Owen Mathieson
+    L - List of Songs
+    A - Add New Song
+    C - Complete A Song
+    Q - Quit"""
+
+    print(menu)
+
+    choice = input(">>> ").upper()
+
+    while choice != "Q":
+        if choice == "L":
+            printsongs = csv.DictReader('Songs.csv', delimiter=',')
+            for line in printsongs:
+                print(line["Song"]),
+                print(line["Artist"])
+                print(line["Year"])
+                print(line["Learned?"])
+
+
+        elif choice == "A":
+            print("Hello")
+
+
+        elif choice == "C":
+            print("Hello")
+
+        else:
+            print("Invalid Option")
+            choice = input(">>> ").upper()
+
+
+main()
